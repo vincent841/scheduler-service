10c258c884b1da3fab5a8b93ad7a72de7ba12924
Jinwon <41100208+vincent841@users.noreply.github.com>
2023-04-20 23:17:44 +0900

    Update schedule_event_type.py

--- src/schedule/schedule_event_type.py
+++ src/schedule/schedule_event_type.py
@@ -5,6 +5,7 @@ from local_crontab import Converter
 import pytz
 
 from schedule.schedule_util import calculate_cron_unit
+from schedule.schedule_cron import CronTime
 
 import sys
 from helper.logger import Logger
@@ -55,33 +56,40 @@ class ScheduleType:
 
     @staticmethod
     def get_next_and_delay(schedule_event, tz):
-        base = datetime.now(timezone.utc)
+        base = datetime.now().astimezone(pytz.timezone(tz))
         if schedule_event["type"] == ScheduleType.CRON:
-            cron_elements = (
-                schedule_event["schedule"].split()
-                if type(schedule_event["schedule"]) is str
-                else str(schedule_event["schedule"]).split()
-            )
+            # cron_elements = (
+            #     schedule_event["schedule"].split()
+            #     if type(schedule_event["schedule"]) is str
+            #     else str(schedule_event["schedule"]).split()
+            # )
 
             next_time = None
             delay = 0
             """
             TODO: check if this routine doesn't have any
             """
-            if len(cron_elements) == 5:
-                utc_cron = Converter(schedule_event["schedule"], tz).to_utc_cron()
-                iter = croniter(utc_cron, base)
-                next_time = datetime.timestamp(iter.get_next(datetime))
-                delay = next_time - datetime.timestamp(base)
-            elif len(cron_elements) == 6:
-                schedule_data = " ".join(cron_elements[1:])
-                utc_cron = Converter(schedule_data, tz).to_utc_cron()
-                iter = croniter(utc_cron, base)
-                next_time = datetime.timestamp(iter.get_next(datetime))
-                secs = calculate_cron_unit(cron_elements[0])
-                delay = next_time - datetime.timestamp(base) + secs
-            else:
-                raise Exception(f'wrong format error: {schedule_event["schedule"]}')
+
+            cron_time = CronTime(schedule_event["schedule"], tz)
+            next_datetime = cron_time.get_next(base, "Asia/Seoul")
+            next_time = next_datetime.timestamp()
+            print(f"cron next time: {next_time}")
+            delay = next_time - datetime.timestamp(base)
+
+            # if len(cron_elements) == 5:
+            #     utc_cron = Converter(schedule_event["schedule"], tz).to_utc_cron()
+            #     iter = croniter(utc_cron, base)
+            #     next_time = datetime.timestamp(iter.get_next(datetime))
+            #     delay = next_time - datetime.timestamp(base)
+            # elif len(cron_elements) == 6:
+            #     schedule_data = " ".join(cron_elements[1:])
+            #     utc_cron = Converter(schedule_data, tz).to_utc_cron()
+            #     iter = croniter(utc_cron, base)
+            #     next_time = datetime.timestamp(iter.get_next(datetime))
+            #     secs = calculate_cron_unit(cron_elements[0])
+            #     delay = next_time - datetime.timestamp(base) + secs
+            # else:
+            #     raise Exception(f'wrong format error: {schedule_event["schedule"]}')
         elif (
             schedule_event["type"] == ScheduleType.DELAY_RECUR
             or schedule_event["type"] == ScheduleType.DELAY
@@ -92,7 +100,9 @@ class ScheduleType:
             next_time = datetime.timestamp(base)
             delay = 0
         elif schedule_event["type"] == ScheduleType.DATE:
-            local_dt = datetime.fromisoformat(schedule_event["schedule"])
+            local_dt = pytz.timezone(tz).localize(
+                datetime.fromisoformat(schedule_event["schedule"])
+            )
             utc_dt = local_dt.astimezone(pytz.UTC)
             next_time = utc_dt.timestamp()
             delay = next_time - datetime.timestamp(base)
